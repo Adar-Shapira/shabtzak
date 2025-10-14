b7c6fc714cc611e4afeb1409cabf2c58e893a0f6
Adar Shapira <adars957@gmail.com>
2025-10-14 17:42:11 +0300

    Update NavBar and main entry to remove old Departments/Roles routes

--- shabtzak-ui/src/components/NavBar.tsx
+++ shabtzak-ui/src/components/NavBar.tsx
@@ -11,13 +11,13 @@ export default function NavBar() {
         <div className="brand">Shabtzak</div>
         <nav className="nav">
           <NavLink to="/" end className={linkClass}>Home</NavLink>
-          <NavLink to="/roles" className={linkClass}>Roles</NavLink>
-          <NavLink to="/departments" className={linkClass}>Departments</NavLink>
+          {/*<NavLink to="/roles" className={linkClass}>Roles</NavLink>*/}
+          {/*<NavLink to="/departments" className={linkClass}>Departments</NavLink>*/}
           <NavLink to="/missions" className={linkClass}>Missions</NavLink>
           <NavLink to="/soldiers" className={linkClass}>Soldiers</NavLink>
           <NavLink to="/planner" className={linkClass}>Planner</NavLink>
           <NavLink to="/assignments" className={linkClass}>Assignments</NavLink>
-          <NavLink to="/vacations" className={linkClass}>Vacations</NavLink>
+          {/*<NavLink to="/vacations" className={linkClass}>Vacations</NavLink>*/}
         </nav>
       </div>
     </header>
--- shabtzak-ui/src/main.tsx
+++ shabtzak-ui/src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Shell from "./layouts/Shell";
 import App from "./App";
-import RolesPage from "./pages/Roles";
+//import RolesPage from "./pages/Roles";
 import MissionsPage from "./pages/Missions";
 import SoldiersPage from "./pages/Soldiers";
 import PlannerPage from "./pages/Planner";
 import AssignmentsPage from "./pages/Assignments";
-import DepartmentsPage from "./pages/Departments";
-import VacationsPage from "./pages/Vacations";
+//import DepartmentsPage from "./pages/Departments";
+//import VacationsPage from "./pages/Vacations";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -17,13 +17,13 @@ const router = createBrowserRouter([
     element: <Shell />,
     children: [
       { index: true, element: <App /> },            // Home
-      { path: "roles", element: <RolesPage /> },
-      { path: "departments", element: <DepartmentsPage /> },
+      //{ path: "roles", element: <RolesPage /> },
+      //{ path: "departments", element: <DepartmentsPage /> },
       { path: "missions", element: <MissionsPage /> },
       { path: "soldiers", element: <SoldiersPage /> },
       { path: "planner", element: <PlannerPage /> },
       { path: "assignments", element: <AssignmentsPage /> },
-      { path: "vacations", element: <VacationsPage /> },
+      //{ path: "vacations", element: <VacationsPage /> },
     ],
   },
 ]);
